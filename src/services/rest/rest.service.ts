614bf64981d7ff34e9ac66877c956836237c006a
derldalfor100 <derldalfor100@gmail.com>
2021-04-14 21:36:19 +0300

    rest service

---
+++ src/services/rest/rest.service.ts
@@ -0,0 +1,83 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { timeout, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) { }
+
+  getRequestAsync(url: string, headers: {[k: string]: string}, params: {[k: string]: string}): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+
+      this.http.get(url, { headers, params })
+      .pipe(timeout(5000),
+      catchError((e: HttpErrorResponse) => {
+        return [{ errorMessageFromRestApi: `Get Request Timeout. ${e.message}`}];
+      })).subscribe(
+        data => {
+          resolve(data);
+        },
+        error => {
+          reject({ errorMessageFromRestApi: `Get Request Error. ${error.message || error}`});
+        });
+    });
+  }
+
+  postRequestAsync(url: string, headers: {[k: string]: string}, params: {[k: string]: string}, body: {[k: string]: string}): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+
+      this.http.post(url, body, { headers, params })
+      .pipe(timeout(5000),
+      catchError((e: HttpErrorResponse) => {
+        return [{ errorMessageFromRestApi: `Post Request Timeout. ${e.message}`}];
+      })).subscribe(
+        data => {
+          resolve(data);
+        },
+        error => {
+          reject({ errorMessageFromRestApi: `Post Request Error. ${error.message || error}`});
+        });
+    });
+  }
+
+  putRequestAsync(url: string, headers: {[k: string]: string}, params: {[k: string]: string}, body: {[k: string]: string}): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+
+      this.http.put(url, body, { headers, params })
+      .pipe(timeout(5000),
+      catchError((e: HttpErrorResponse) => {
+        return [{ errorMessageFromRestApi: `Put Request Timeout. ${e.message}`}];
+      })).subscribe(
+        data => {
+          resolve(data);
+        },
+        error => {
+          reject({ errorMessageFromRestApi: `Put Request Error. ${error.message || error}`});
+        });
+    });
+  }
+
+  deleteRequestAsync(url: string, headers: {[k: string]: string}, params: {[k: string]: string}): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+
+      this.http.delete(url, { headers, params })
+      .pipe(timeout(5000),
+      catchError((e: HttpErrorResponse) => {
+        return [{ errorMessageFromRestApi: `Delete Request Timeout. ${e.message}`}];
+      })).subscribe(
+        data => {
+          resolve(data);
+        },
+        error => {
+          reject({ errorMessageFromRestApi: `Delete Request Error. ${error.message || error}`});
+        });
+    });
+  }
+}
